fe005f39f2be20e7ecdadba8a84b20cbc4adefad
SartoRiccardo <rikki.sarto@gmail.com>
2020-03-11 20:20:08 +0100

    ^0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scpython",
-    version="0.2.0",
+    version="0.1.0",
     author="TheSartorsss",
     description="A package to fetch data from the SCP Foundation",
     long_description=long_description,
